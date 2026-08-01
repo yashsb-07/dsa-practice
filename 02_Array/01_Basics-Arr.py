797d7f04daf299fc76702b031e289aab67d8bb8b
yashsb-07 <yashsbansode07@gmail.com>
2026-08-01 11:24:59 +0530

    Learn Array in python using Array Module

---
+++ 02_Array/01_Basics-Arr.py
@@ -0,0 +1,45 @@
+# USING ARRAY MODULE
+
+# from array import *
+
+# val = array('i', [1,2,3,4,5,6,7])
+
+# copyVal = array(val.typecode, (x*3 for x in val))
+
+# copyVal.remove(15)
+# copyVal.pop(3)
+# copyVal.pop()
+
+# for i in range(0, len(copyVal)):
+#     print(copyVal[i], end=" ")
+
+
+#1
+# for i in range(0, 6): 
+    # print(val[i], end=" ")
+
+#2
+# for i in val:
+#     print(i, end=" ")
+
+# print(val)
+
+# Slicing
+
+# newArr = val[2:6]
+
+# for i in range(0, len(newArr)):
+#     print(newArr[i], end=" ")
+
+# arr = array("i", [])
+
+# n = int(input("Enter a num: "))
+
+# for i in range(0, n):
+#     arr.append(int(input("Enter next number: ")))
+
+# for i in range(0, len(arr)):
+#     print(arr[i], end=" ")
+
+
+# USING NUMPY 
