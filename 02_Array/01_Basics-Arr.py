d85319c98ef581b6cfca3cacebb8b0c40dad0ab3
yashsb-07 <yashsbansode07@gmail.com>
2026-08-01 12:10:38 +0530

    Learn Array in python using numpy

--- 02_Array/01_Basics-Arr.py
+++ 02_Array/01_Basics-Arr.py
@@ -43,3 +43,24 @@
 
 
 # USING NUMPY 
+
+from numpy import *
+
+# val = array([1,2,3,4,5])
+
+# for x in val:
+#     print(x, end=" ")
+
+# Zero Dimentional
+
+zero = array(10)
+# print(zero)
+
+one = array([1,2,3,4,5])
+# print(one)
+
+two = array([[1,2,3], [4,5,6], [7,8,9]])
+# print(two)
+
+three = array([ [ [1,2], [3,4]], [ [5,6], [7,8] ] ])
+print(three)
